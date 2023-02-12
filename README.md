65205c3663c16dc90072d4f8d7605ce48f5e3e81
Yuri Solovev <solovevym@gmail.com>
2023-02-12 10:40:57 +0300

    task #2

---
+++ README.md
@@ -0,0 +1,2 @@
+skillfactory module C5 homework
+1. XML parcing
---
+++ task_2/script.js
@@ -0,0 +1,20 @@
+const jsonSting = `
+{
+  "list": [
+   {
+    "name": "Petr",
+    "age": "20",
+    "prof": "mechanic"
+   },
+   {
+    "name": "Vova",
+    "age": "60",
+    "prof": "pilot"
+   }
+  ]
+}
+`;
+
+const data = JSON.parse(jsonSting);
+
+console.log(data);
